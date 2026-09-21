fa1681c3d59eb28d997f655621f44f90b46285b6
jwnuk <julia.m.wnuk@gmail.com>
2026-09-21 22:40:30 +0200

    Restore state before debugging

--- main.py
+++ main.py
@@ -115,8 +115,6 @@ def get_event_details(page, url: str) -> dict:
         )
         location = sections.nth(2).inner_text()
 
-        print("if event_header.count() returned True")
-
         return {
             "Data": date_time,
             "Dzień tygodnia": weekday,
@@ -145,7 +143,6 @@ def get_event_details(page, url: str) -> dict:
         )
 
         if date_index is None:
-            print("date_index is None")
             return {
                 "Data": None,
                 "Dzień tygodnia": None,
@@ -158,7 +155,6 @@ def get_event_details(page, url: str) -> dict:
         title = lines[date_index + 1]
         location = lines[date_index + 2]
 
-        print("if event_header.count() returned False (else statement)")
         return {
             "Data": date_time,
             "Dzień tygodnia": weekday,
@@ -214,7 +210,6 @@ def get_events_from_fb(organiser: dict, is_group: bool = False) -> list[dict]:
                 event_details.append(details)
             # break
 
-            print(details)
         browser.close()
 
     return event_details
@@ -258,14 +253,11 @@ if __name__ == "__main__":
         "URL",
     ]
     df = pd.DataFrame(all_events, columns=column_order)
-    print(f"Non-empty dates before parsing: {df['Data'].notna().sum()}")
+
     df["Data"] = df["Data"].apply(parse_date)
-    print(f"Non-empty dates after parsing: {df['Data'].notna().sum()}")
 
     df = df.sort_values("URL", key=lambda x: x.str.len())
     df = df.drop_duplicates(subset=["Data", "Tytuł", "Lokalizacja"], keep="first")
-    print("Events before filtering:", len(df))
     df = df[df["Data"] >= datetime.datetime.today()]
-    print("Events after filtering:", len(df))
 
     df.sort_values(["Data", "Organizator", "Tytuł"]).to_csv("events.csv", index=False)
